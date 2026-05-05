79e31ef5e9b486b7c5d1d824d8249027ae2a8337
Brian Milian <brianmilian@csu.fullerton.edu>
2026-05-04 20:40:23 -0700

    Print duplicates in order of first appearance

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def main():
     # from here you can update the size of the table
     table_size = 30
     table = [[] for _ in range(table_size)]
+    ##keeping_track_of_order = []
 
     # pseudocode:
     # my pseudocode has changed a couple of times
@@ -36,6 +37,7 @@ def main():
         # if element is not at hash_index
         if not table[hash_index]:
             table[hash_index] = [element, 1]
+            ##keeping_track_of_order.append([element, 1])
 
         # if element does NOT equal element at hash_index
         #         [   row    ][index at row][key]
@@ -49,6 +51,7 @@ def main():
             # if element not found in row, add it to the end of row
             if found == False:
                 table[hash_index][0].append([element, 1])
+                ##keeping_track_of_order.append([element, 1])
         
         # if element DOES EQUAL element at hash_index, increment value by 1
         else:
@@ -60,6 +63,27 @@ def main():
     # print duplicates and appearances
     # print total unique IDs
     # print total duplicate IDs
+
+    # for row in table:
+    #     for bucket in row:
+    #         if bucket[1] > 1:
+    #             keeping_track_of_order.append([bucket[0], bucket[1]])
+    
+    # we basically loop through IDs a second time to print duplicates with their occurences (in order of first appearance)
+    print("Duplicates found (in order of first appearance): ")
+    for element in identifications:
+        hash_index = my_hash_function(element, table_size)
+        # we look for element in row if not in the first index
+        #         [   row    ][index at row][key]
+        if table[hash_index][       0    ][ 0 ] != element:
+            # look for element in row
+            for item in table[hash_index]:
+                if item[0] == element and item[1] > 1:
+                    print(element, " -> appears ", item[1], " times")
+        # we print element only if it is a duplicate 
+        #         [   row    ][index at row][key]
+        if table[hash_index][       0    ][ 0 ] == element and table[hash_index][       0    ][ 1 ] > 1:
+            print(element, " -> appears ", item[1], " times")      
   
 
     # write to output
