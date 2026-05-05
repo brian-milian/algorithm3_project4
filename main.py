ff4bf01b3a14e05ca3ff3ae84dbee11c44736e40
Brian Milian <brianmilian@csu.fullerton.edu>
2026-05-04 21:24:46 -0700

    Program print to output.txt

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import re
+
+
+
 def read_input_file(filename):
     with open(filename, "r") as file:
         data = file.read()
@@ -5,16 +9,19 @@ def read_input_file(filename):
 
 
 def write_output_file(filename, output):
-    with open(filename, "w") as file:
+    with open(filename, "a") as file:
         file.write(output)
 
 
 def main():
+    with open("output.txt", "w") as file:
+        pass
+
     # read input
     data = read_input_file("in4c.txt")
 
     # convert input text to list of ints
-    identifications = list(map(int, data.split()))
+    identifications = list(map(int, re.findall(r"\d+", data)))
 
     # my algorithm
 
@@ -36,12 +43,12 @@ def main():
 
         # if element is not at hash_index
         if not table[hash_index]:
-            table[hash_index] = [element, 1]
+            table[hash_index]. append([element, 1])
             ##keeping_track_of_order.append([element, 1])
 
         # if element does NOT equal element at hash_index
         #         [   row    ][index at row][key]
-        elif table[hash_index][       0    ][ 0 ] != element:
+        elif table[hash_index][0][0] != element:
             found = False
             # look for element in row
             for item in table[hash_index]:
@@ -56,7 +63,7 @@ def main():
         # if element DOES EQUAL element at hash_index, increment value by 1
         else:
             #    [   row    ][index at row][value]
-            table[hash_index][      0     ][  1  ] += 1
+            table[hash_index][0][1] += 1
 
 
     # now we just have to loop through table and print output as mentioned in document
@@ -70,7 +77,7 @@ def main():
     #             keeping_track_of_order.append([bucket[0], bucket[1]])
     
     # we basically loop through IDs a second time to print duplicates with their occurences (in order of first appearance)
-    write_output_file("output.txt", "Duplicates found (in order of first appearance): ")
+    write_output_file("output.txt", "Duplicates found (in order of first appearance): \n")
     for element in identifications:
         hash_index = my_hash_function(element, table_size)
         # we look for element in row if not in the first index
@@ -79,11 +86,11 @@ def main():
             # look for element in row
             for item in table[hash_index]:
                 if item[0] == element and item[1] > 1:
-                    write_output_file("output.txt", element, " -> appears ", item[1], " times")
+                    write_output_file("output.txt", f"{element} -> appears {item[1]} times")
         # we print element only if it is a duplicate 
         #         [   row    ][index at row][key]
-        if table[hash_index][       0    ][ 0 ] == element and table[hash_index][       0    ][ 1 ] > 1:
-            write_output_file("output.txt", element, " -> appears ", item[1], " times")
+        if table[hash_index][0][0] == element and table[hash_index][0][1] > 1:
+            write_output_file("output.txt", f"{element} -> appears {table[hash_index][0][1]} times")
 
   
 
